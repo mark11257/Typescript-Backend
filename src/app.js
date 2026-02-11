e42386d3739bb084df1f35ee109b94227e14c753
mark rafat <markrafat1@gmail.com>
2026-02-12 00:04:38 +0200

    made an invoice class

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
+//Inputs
 var button = document.querySelector(".button1");
-//console.log(`${button.innerHTML}, ${button.style}`);
+var form = document.querySelector(".new-item-form");
+var type = document.querySelector("#type");
+var tofrom = document.querySelector("#tofrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
+//classes
+var Invoice = /** @class */ (function () {
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    Invoice.prototype.print = function () {
+        console.log("Client ".concat(this.client, " owes $").concat(this.amount, " for ").concat(this.details));
+    };
+    return Invoice;
+}());
+// test button
 function text() {
     button.innerHTML = "clicked";
 }
@@ -7,11 +25,10 @@ function text() {
 button.addEventListener("click", function () {
     text();
 });
-var form = document.querySelector(".new-item-form");
-var type = document.querySelector("#type");
-var tofrom = document.querySelector("#tofrom");
-var details = document.querySelector("#details");
-var amount = document.querySelector("#amount");
+//const invoice = new Invoice(tofrom, details, amount);
+var invOne = new Invoice("Mario", "Website work", 200);
+console.log(invOne);
+//Handle form data
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,31 @@
+//Inputs
 const button= document.querySelector(".button1") as HTMLButtonElement;
-//console.log(`${button.innerHTML}, ${button.style}`);
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+const type = document.querySelector("#type") as HTMLInputElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+//classes
+
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+    
+
+    constructor (client:string,details:string,amount:number){
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    print() {
+        console.log(`Client ${this.client} owes $${this.amount} for ${this.details}`);    
+    }
+
+}
+
+// test button
 function text():any{
     button.innerHTML = "clicked";
 };
@@ -7,13 +33,10 @@ function text():any{
 button.addEventListener("click", function(){
     text();
 });
-
-const form = document.querySelector(".new-item-form") as HTMLFormElement;
-const type = document.querySelector("#type") as HTMLInputElement;
-const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
-const details = document.querySelector("#details") as HTMLInputElement;
-const amount = document.querySelector("#amount") as HTMLInputElement;
-
+//const invoice = new Invoice(tofrom, details, amount);
+ const invOne = new Invoice("Mario", "Website work", 200);
+ console.log(invOne);
+//Handle form data
 form.addEventListener("submit",(e: Event) => {
     e.preventDefault();
     console.log(type.value, tofrom.value , details.value, amount.valueAsNumber);
