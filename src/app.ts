30da43d0b09fb6fa655269d094d4974e8b8f1346
mark rafat <markrafat1@gmail.com>
2026-02-11 14:54:47 +0200

    testing new setup installed

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,9 @@
-let greet: (name:string, age:number)=>void;
-greet  = (name:string, age:number) =>{
-    console.warn(`Hello ${name} you are ${age} years old`);
-}
-
-greet('Mark',23);
-
-let attack:(damage: number, health:number)=>number;
-attack= (damage: number, health:number)=>{
-    return health - damage;
-}
-
-let attackk = attack(10,100);
-console.log(`Beast current health is ${attackk}`)
-
-type man ={
-    name: string, 
-    age: number
+const button= document.querySelector(".button1") as HTMLButtonElement;
+//console.log(`${button.innerHTML}, ${button.style}`);
+function text():any{
+    button.innerHTML = "clicked";
 };
 
-let greeet:(obj: man)=>void;
-greeet = (human:man)=>{
-    console.log(`${human.name} you are of age ${human.age}`)
-}
-let humann: man ={
-    name:'Mark',
-    age:24
-};
-greeet(humann);
+button.addEventListener("click", function(){
+    text();
+});
