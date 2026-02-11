ba9f1b8b9475a722a8bf6e516613d6ed39890c55
mark rafat <markrafat1@gmail.com>
2026-02-11 20:51:46 +0200

    added static data logged to console

--- src/app.js
+++ src/app.js
@@ -7,3 +7,12 @@ function text() {
 button.addEventListener("click", function () {
     text();
 });
+var form = document.querySelector(".new-item-form");
+var type = document.querySelector("#type");
+var tofrom = document.querySelector("#tofrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
--- src/app.ts
+++ src/app.ts
@@ -7,3 +7,14 @@ function text():any{
 button.addEventListener("click", function(){
     text();
 });
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+const type = document.querySelector("#type") as HTMLInputElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit",(e: Event) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value , details.value, amount.valueAsNumber);
+});
